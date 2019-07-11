c7b07551db5389ccda2d94443f03aad21a35bb03
jaojeda <j.a.ojeda@live.com>
2019-07-10 19:55:27 -0700

    created simple app

---
+++ app.js
@@ -0,0 +1,9 @@
+
+//Reference needed DOM elements
+const result = document.getElementById('result');
+const nameInput = document.getElementById('name');
+
+// Action to take on user event
+function updateResult() {
+    result.textContent = nameInput.value;
+}
